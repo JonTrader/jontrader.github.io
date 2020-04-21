270e92b1fe74b5e16d367a45542a41f06e89eb3c
Jonathan Montoya <montoya0396@gmail.com>
2020-04-20 21:38:38 -0400

    Updates

---
+++ precache-manifest.530b77a35659c7af5380006f580004ba.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b58a5bf84fe86212f247781f5e35fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5fa92c17f6943902072",
+    "url": "/static/css/main.40213550.chunk.css"
+  },
+  {
+    "revision": "77273271392a459abf5d",
+    "url": "/static/js/2.e8910171.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e8910171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5fa92c17f6943902072",
+    "url": "/static/js/main.84eb05d5.chunk.js"
+  },
+  {
+    "revision": "b31033d1557dcb267ec2",
+    "url": "/static/js/runtime-main.050dfbab.js"
+  },
+  {
+    "revision": "8ec908094ae2c2c7a8bf1c2532d5d7a8",
+    "url": "/static/media/htmlcss.8ec90809.png"
+  },
+  {
+    "revision": "3c2b73676466d257b782ba05eae6bb39",
+    "url": "/static/media/jonathanmontoya.3c2b7367.png"
+  },
+  {
+    "revision": "4a9ff96c98692f77ee3611f45658f55c",
+    "url": "/static/media/project1.4a9ff96c.gif"
+  },
+  {
+    "revision": "7f89eae22bc476ed9b2d4167b87aba17",
+    "url": "/static/media/project2.7f89eae2.gif"
+  },
+  {
+    "revision": "510540566f32727b16000b5372edfc0b",
+    "url": "/static/media/project3.51054056.gif"
+  }
+]);
