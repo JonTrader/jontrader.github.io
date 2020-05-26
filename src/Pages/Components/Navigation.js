a46159fb49779c81eca27d10ae1ef8a67c494f0c
Jonathan Montoya <montoya0396@gmail.com>
2020-05-25 23:50:31 -0400

    added bootstrap 4 and migrated design of welcome page and navbar to bootstrap and now mobile friendly

--- src/Pages/Components/Navigation.js
+++ src/Pages/Components/Navigation.js
@@ -1,32 +1,39 @@
 import React from 'react';
 import jlogo from '../Images/j.png';
-import '../CSS/Navigation.css';
-import { Link } from "react-router-dom";
+import '../CSS/NavBar.css';
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
 
-export default function Nav()
+export default class Navigation extends React.Component
 {
-    return (
-        <header className="nav-bar">
-            <div className="nav-box">
-               <Link to="/home">
-                  <img src={jlogo} className="nav-logo" alt=""/>
-                  <h3 className="nav-jon nav-logotext">Jonathan Montoya</h3>
-                  <h3 className="nav-webdev nav-logotext">Web Developer</h3>
-               </Link>
-               <nav>
-                  <ul>
-                     <li>
-                        <Link to="/about" className="about-link nav-links underline">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/projects" className="projects-link nav-links">Projects</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact" className="contact-link nav-links">Contact</Link>
-                    </li>
-                </ul>
-               </nav>
-            </div>
-         </header>
-      );
+    render()
+    {
+        return (
+
+                <Navbar expand="lg" variant="dark" className="navBarAnimation">
+
+                    <Container>
+                        <Navbar.Brand href="/home">
+                            <img src={jlogo} className="navLogo" alt=""/>
+                        </Navbar.Brand>
+
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" className="lines" />
+
+                        <Navbar.Collapse id="basic-navbar-nav">
+
+                            <Nav className="ml-auto">
+                                <Nav.Link href="/about" className="mr-4">About</Nav.Link>
+                                <Nav.Link href="/projects" className="mr-4">Projects</Nav.Link>
+                                <Nav.Link href="/contact">Contact</Nav.Link>
+                            </Nav>
+
+                        </Navbar.Collapse>
+                    </Container>
+
+                </Navbar>
+            
+        );
+    }
+
 }
--- src/Pages/Components/Welcome.js
+++ src/Pages/Components/Welcome.js
@@ -2,27 +2,44 @@ import React from 'react';
 import jlogo from '../Images/j.png';
 import '../CSS/Welcome.css';
 import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 export default class Welcome extends React.Component {
-
 
     render() {
         
         return (
 
+            <Container className="text-center">
 
-        <div className="welcome">
-            <div className="welcome-box">
-                <img src={jlogo} alt="" className="welcome-logo"/>
-                <p className="intro-p"> Hello, I'm <span className="jonmon">Jonathan Montoya.</span></p>
-                <p className="intro-p">I'm happy you're here!</p>
-            </div>
-            <Link to="/home" className="welcome-link">
-                <button className="welcome-btn">
-                    Lets Get Started!
-                </button>
-            </Link>
-        </div>
+                <Row id="mtCustom">
+                    <Col>
+                        <img src={jlogo} alt="" className="welcomeLogo"/>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col className="text-white welcomeText">
+                        <h1>
+                            Hello, I'm <span>Jonathan Montoya</span>
+                        </h1>
+                        <h4>I'm happy you're here!</h4>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                        <Link to="/home">
+                            <button className="welcomeBtn">
+                                Lets Get Started!
+                            </button>
+                        </Link>
+                    </Col>
+                </Row>
+
+            </Container>
         );
     }   
 
